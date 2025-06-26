53e0a8899b253e2cb41778e99d5668a65f3a1f96
Atharv Kumbhar <ak1074834@gmail.com>
2025-06-27 02:40:10 +0530

    Resolve merge

--- admin/config.py
+++ admin/config.py
@@ -15,13 +15,6 @@ class Config:
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'ak1074834@gmail.com'
-    MAIL_PASSWORD = '/ask.in/kumbhar'
-
-    # Stripe
-    ***REMOVED***
-    ***REMOVED***
-
     # File Uploads
     UPLOAD_FOLDER = 'static/uploads/'
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
