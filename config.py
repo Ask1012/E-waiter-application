2f68ee9f48c09b30a1b0a903e8d1790c086f8e3d
Ask1012 <ak1074834@gmail.com>
2025-07-09 14:14:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,12 +3,8 @@ from datetime import timedelta
 
 class Config:
     SECRET_KEY =  os.getenv("SECRET_KEY", "dev-secret") 
-    SQLALCHEMY_DATABASE_URI =  os.getenv('SQLALCHEMY_DATABASE_URI', 'mysql+pymysql://root:password@localhost:3306/hotel')
-    SQLALCHEMY_DATABASE_URI = (
-    f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
-    f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
-    f"?ssl_ca=certs/isrgrootx1.pem"
-)
+    SQLALCHEMY_DATABASE_URI =  os.getenv('DB_CONNECTION_STR')
+    
 
     STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
